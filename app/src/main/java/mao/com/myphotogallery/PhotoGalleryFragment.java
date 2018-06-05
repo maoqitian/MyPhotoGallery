ee618d00ce58e87336ff608c29d6c01b2afd0ef2
maoqitian <532977849@qq.com>
2018-06-05 23:45:49 +0800

    parse json data

--- app/src/main/java/mao/com/myphotogallery/PhotoGalleryFragment.java
+++ app/src/main/java/mao/com/myphotogallery/PhotoGalleryFragment.java
@@ -10,10 +10,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.io.FileFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import mao.com.myphotogallery.http.FlickrFetchr;
+import mao.com.myphotogallery.model.GalleryItem;
 
 /**
  * 显示图片Fragment
@@ -22,6 +27,8 @@ public class PhotoGalleryFragment extends Fragment{
 
     private RecyclerView mPhotoRecyclerView;
     private static final String TAG = "PhotoGalleryFragment";
+
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,7 +45,14 @@ public class PhotoGalleryFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mPhotoRecyclerView=view.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        setupAdapter();
         return view;
+    }
+
+    private void setupAdapter() {
+        if (isAdded()) {//isAdded()检查确认fragment已与目 标activity相关联，从而保证getActivity()方法返回结果非空
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
     }
 
     public static PhotoGalleryFragment newInstance() {
@@ -52,17 +66,49 @@ public class PhotoGalleryFragment extends Fragment{
     }
 
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void>{
+    private class FetchItemsTask extends AsyncTask<Void,Void, List<GalleryItem> >{
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            try {    String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            } catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
-            return null;
+        protected  List<GalleryItem>  doInBackground(Void... voids) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            setupAdapter();
         }
     }
 
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+        public void bindGalleryItem(GalleryItem item)
+        {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> mGalleryItems;
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;   }
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+        {   TextView textView = new TextView(getActivity());   return new PhotoHolder(textView);
+        }
+        @Override
+        public void onBindViewHolder(PhotoHolder photoHolder, int position)
+        {   GalleryItem galleryItem = mGalleryItems.get(position);
+        photoHolder.bindGalleryItem(galleryItem);
+        }
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
 }
--- app/src/main/java/mao/com/myphotogallery/http/FlickrFetchr.java
+++ app/src/main/java/mao/com/myphotogallery/http/FlickrFetchr.java
@@ -1,36 +1,50 @@
 package mao.com.myphotogallery.http;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import mao.com.myphotogallery.model.GalleryItem;
 
 /**
  * 基本网络连接
  */
 public class FlickrFetchr {
 
-    //获取网络数据字节数组
-    public byte[] getUrlBytes(String urlSpec ) throws IOException{
-        URL url=new URL(urlSpec);
-        HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
-        try{
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = " f2c037ce818631eab099ef43e54c3165 ";
 
-            ByteArrayOutputStream baos=new ByteArrayOutputStream();
+    //获取网络数据字节数组
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        try {
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
             InputStream inputStream = httpURLConnection.getInputStream();
-            if(httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK){//连接不成功
-                throw new IOException(httpURLConnection.getResponseMessage() +  ": with " +  urlSpec);
+            if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {//连接不成功
+                throw new IOException(httpURLConnection.getResponseMessage() + ": with " + urlSpec);
             }
-            int bytesRead=0;
-            byte[] buffer=new byte[1024];
-            while((bytesRead=inputStream.read(buffer))>0){
-                baos.write(buffer,0,bytesRead);
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = inputStream.read(buffer)) > 0) {
+                baos.write(buffer, 0, bytesRead);
             }
             baos.close();
             return baos.toByteArray();
-        }finally {
-           httpURLConnection.disconnect();
+        } finally {
+            httpURLConnection.disconnect();
         }
     }
 
@@ -38,4 +52,49 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
+    /**
+     * 获取GalleryItem 数据
+     */
+    public  List<GalleryItem>  fetchItems() {
+        List<GalleryItem> items = new ArrayList<>();
+        try {
+        String url = Uri.parse("https://api.flickr.com/services/rest/")
+                .buildUpon()
+                .appendQueryParameter("method", "flickr.photos.getRecent")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .appendQueryParameter("extras", "url_s")
+                .build().toString();
+            String jsonStr=getUrlString(url);
+            JSONObject jsonObject=new JSONObject(jsonStr);
+            Log.i(TAG, "Received JSON: " + jsonStr);
+            parseItems(items,jsonObject);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    /**
+     * 解析JSON 数据
+     * @param items
+     * @param jsonBody
+     */
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException,JSONException {
+
+        JSONObject photosJsonObject=jsonBody.getJSONObject("photos");
+        JSONArray photojsonArray=photosJsonObject.getJSONArray("photo");
+        for (int i = 0; i < photosJsonObject.length(); i++) {
+            GalleryItem galleryItem=new GalleryItem();
+            JSONObject photoJsonObject  = photojsonArray.getJSONObject(i);
+            galleryItem.setmCaption(photoJsonObject.getString("title"));
+            galleryItem.setmId(photoJsonObject.getString("id"));
+            if(!photoJsonObject.has("url_s")){
+               continue;
+            }
+            galleryItem.setmUrl(photoJsonObject.getString("url_s"));
+            items.add(galleryItem);
+        }
+    }
 }
